7123625e44d4a6ce2551e70628663b8a986f0067
Danielslimaa <dslima793@gmail.com>
2024-04-15 21:30:11 -0300

    - first commit

---
+++ main.py
@@ -0,0 +1,28 @@
+import numpy as np 
+import matplotlib.pyplot as plt
+import math
+import scipy 
+from scipy import special
+import scipy.special as sc
+from sympy import *
+import time
+from time import strftime, gmtime
+import numba
+from numba import jit
+import pandas as pd
+import sys
+
+import concurrent.futures
+import multiprocessing
+num_processes = multiprocessing.cpu_count()
+
+from timeit import default_timer as timer
+
+import sympy
+
+font = {'family': 'serif',
+          'color':  'black',
+          'weight': 'normal',
+          'size': 16,
+          }
+
