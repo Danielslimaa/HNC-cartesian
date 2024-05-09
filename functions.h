e17d654479fb64ed67155470e94f17b5decb8a04
Danielslimaa <dslima793@gmail.com>
2024-05-09 16:41:34 -0300

    -

--- functions.h
+++ functions.h
@@ -4,7 +4,7 @@
 extern long int N;
 extern double inv_N2;
 extern double L;
-extern double h;
+extern double dx, dy, dkx, dky;
 extern double U;
 extern double rho;
 extern double dt;
@@ -29,8 +29,8 @@ void geometry(double * x, double * y, double * kx, double * ky, double * k2)
   {
     for (int j = 0; j < N; j++)
     {
-      x[i * N + j] = (-L / 2) + (i - 1) * h;
-      y[i * N + j] = (-L / 2) + (j - 1) * h;
+      x[i * N + j] = (-L / 2) + (i - 1) * dx;
+      y[i * N + j] = (-L / 2) + (j - 1) * dy;
     }
   }
 
@@ -76,27 +76,83 @@ void potential_U(double * x, double * y, double * V)
   }
 }
 
-void compute_Vph(double * k2, double * S, double * Vph)
+void compute_omega(fftw_plan omega_to_omega, double * k2, double * S, double * omega)
 {
+  double c = dkx * dky * ( 1.0 / (2. * M_PI * 2.0 * M_PI * rho) ) * inv_N2;
   #pragma omp parallel for 
   for (int i = 0; i < N * N; i++)
   {
-    Vph[i] = -0.25 * k2[i] * ( 2. * S[i] + 1. ) * ( 1. - (1. / S[i]) ) * ( 1. - (1. / S[i]) ); 
+    omega[i] = - c * 0.25 * k2[i] * ( 2. * S[i] + 1. ) * ( 1. - (1. / S[i]) ) * ( 1. - (1. / S[i]) ); 
   }  
+  fftw_execute(omega_to_omega);
 }
 
-double compute_error(fftw_complex * new_g, fftw_complex * g)
+double compute_error(double * new_g, double * g)
 {
   double sum, tmp= 0;
   #pragma omp parallel for reduction(+ : sum) private(tmp)
   for (int i = 0; i < N * N; i++)
   {
-    tmp = abs(new_g[i][0] - g[i][0]); 
+    tmp = abs(new_g[i] - g[i]); 
     sum += tmp; 
   } 
-  return sum * h * h / dt;
+  return sum * dx * dy / dt;
 }
 
+void laplace(double * g, double * Lg)
+{
+  double inv_hh = 1. / (dx * dx); 
+  #pragma omp parallel for 
+  for (int i = 0; i < N / 2; i++)
+  {
+    for (int j = 0; j < N / 2; j++)
+    {
+      Lg[i * N + j] = g[i * N + j + 1] - g[i * N + j] + g[i * N + j - 1]; // del_del_x
+      Lg[i * N + j] += g[(i + 1) * N + j] - g[i * N + j] + g[(i - 1) * N + j]; // del_del_y
+      Lg[i * N + j] *= inv_hh;
+      Lg[(N - 1 - i) * N + j] = Lg[i * N + j];
+      Lg[i * N + (N - 1 - j)] = Lg[i * N + j];
+      Lg[(N - 1 - i) * N + (N - 1 - j)] = Lg[i * N + j];
+    } 
+  } 
+}
+
+void update_g(double * Lg, double * V, double * omega, double * g)
+{
+  #pragma omp parallel for
+  for (int i = 0; i < N * N; i++)
+  {
+    g[i] = (1. - dt) * g[i] + dt * (-Lg[i] + V[i] * g[i] + omega[i] * g[i]); //Eq.(7) 
+  }
+  return;
+}
+
+void compute_S(fftw_plan g_to_S, double * g, double * new_S)
+{
+  #pragma omp parallel for
+  for (int i = 0; i < N * N; i++)
+  {
+    new_S[i] = g[i] * g[i] - 1.0; //Because g[] = sqrt{g[]}
+  }
+  fftw_execute(g_to_S); //Actually it is a FFTW_REDFT00 made inplace with new_S -> new_S
+  #pragma omp parallel for
+  for (int i = 0; i < N * N; i++)
+  {
+    new_S[i] = 1.0 + rho * new_S[i] * dkx * dky * inv_N2;
+  }
+  return;  
+}
+
+void initialize_g_S(double * g, double * S)
+{
+  #pragma omp parallel for
+  for (int i = 0; i < N * N; i++)
+  {
+    S[i] = 1.0;
+    g[i] = 1.10;
+  }
+  return;  
+}
 
 
 #endif
--- main.cpp
+++ main.cpp
@@ -12,7 +12,7 @@
 long int N;
 double inv_N2;
 double L; 
-double h;
+double dx, dy, dkx, dky;
 double U;
 double rho;
 double dt;
@@ -20,36 +20,96 @@ double dt;
 
 int main(void)
 {
-  N = 1 << 8;
+  N = 1 << 7;
   L = 40.;
-  h = L / (double)N;
+  dx = L / (double)N;
+  dy = L / (double)N;
+  inv_N2 = 1. / ((double)(N * N));
+  dkx = 2. * M_PI / (L * (double)N);
+  dky = 2. * M_PI / (L * (double)N);
 
   U = 10;
   rho = 1;
+  dt = 0.001;
+  printf("U = %1.2f, rho = %1.2f, dt = %1.4f", U, rho, dt);
+  int max_threads = omp_get_max_threads();
+  printf("Maximum number of threads = %d\n", max_threads);
+  omp_set_num_threads(max_threads);
 
-  double * x, * y, * kx, * ky, * k2, * V, * g, * S, * Vph;
-  x = (double * )fftw_malloc(N * N * sizeof(double));
-  y = (double * )fftw_malloc(N * N * sizeof(double));
-  kx = (double * )fftw_malloc(N * N * sizeof(double));
-  ky = (double * )fftw_malloc(N * N * sizeof(double));
-  k2 = (double * )fftw_malloc(N * N * sizeof(double));
-  V = (double * )fftw_malloc(N * N * sizeof(double));
-  g = (double * )fftw_malloc(N * N * sizeof(double));
-  S = (double * )fftw_malloc(N * N * sizeof(double));
-  Vph = (double * )fftw_malloc(N * N * sizeof(double));
+  int fftw_init_threads(void);
+  fftw_plan_with_nthreads(max_threads);
+
+  double * x = new double[N * N];
+  double * y = new double[N * N];
+  double * kx = new double[N * N];
+  double * ky = new double[N * N];
+  double * k2 = new double[N * N];
+  double * V = new double[N * N];
+  double * g = new double[N * N];
+  double * S = new double[N * N];
+  double * new_S = new double[N * N];
+  double * omega = new double[N * N];
+  double * Lg = new double[N * N];
+
+  unsigned flags;
+  bool with_wisdom = false;
+  if(with_wisdom)
+  {
+    flags = FFTW_WISDOM_ONLY;
+    printf ("Importing wisdom \n");
+    char import_buffer[200];
+    sprintf(import_buffer,"N%d_fftw3.wisdom", N);
+    int numberwisdom = fftw_import_wisdom_from_filename(import_buffer);
+  }
+  else
+  {
+    fftw_set_timelimit(FFTW_NO_TIMELIMIT);
+    flags = FFTW_PATIENT;
+    printf("Setting FFT plans.\n");
+  }
+
+  fftw_plan g_to_S =  fftw_plan_r2r_2d(N, N, new_S, new_S, FFTW_REDFT00, FFTW_REDFT00, flags);
+  fftw_plan omega_to_omega =  fftw_plan_r2r_2d(N, N, omega, omega, FFTW_REDFT00, FFTW_REDFT00, flags);
 
   geometry(x, y, kx, ky, k2);
   potential_U(x, y, V);  
   printer_vector(x, y, V, "V.dat");
+  initialize_g_S(g, S);
 
   bool condition = true; 
-
+  double error = 1.;
+  long int counter = 1;
   while(condition)
   {
-    
+    laplace(g, Lg);
+    compute_omega(omega_to_omega, k2, S, omega);
+    update_g(Lg, V, omega, g);
+    compute_S(g_to_S, g, new_S);
+    if(counter%100 == 0 or counter == 1)
+    {
+      error = compute_error(new_S, S);
+      printf("t = %ld, error = %1.4e\n", counter, error);
+      if(counter > 1)
+      {
+        condition = (error > 1e-6);
+      }
+    }
+    memcpy(S, new_S, N * N * sizeof(double));
+    counter += 1;
   }
 
   void fftw_cleanup_threads(void);
   fftw_cleanup();
+  delete[] x;
+  delete[] y;
+  delete[] kx;
+  delete[] ky;
+  delete[] k2;
+  delete[] V;
+  delete[] g;
+  delete[] S;
+  delete[] new_S;
+  delete[] omega;
+  delete[] Lg;
   return 0;
 }
