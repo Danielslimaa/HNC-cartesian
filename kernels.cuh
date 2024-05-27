bf9176116c37dc31a152a5e2c19a4b03764ab261
DanielSlimaa <dslima793@gmail.com>
2024-05-26 22:17:10 -0300

    -

--- kernels.cuh
+++ kernels.cuh
@@ -192,3 +192,49 @@ __global__ void rescaling(double * U)
         U[i] /= (double)(N * N);
     }
 }
+
+__global__ void g_from_S3(
+	double *__restrict__ g,
+	const double *__restrict__ S,
+	double *sum_diff_g)
+{
+	const unsigned int tid = threadIdx.x + blockIdx.x * blockDim.x;
+
+	double x_shfl_src, x_shfl_dest;
+
+	double y_val = 0.0;
+
+#pragma unroll
+	for (unsigned int m = 0; m < ((N + BLOCK_SIZE - 1) / BLOCK_SIZE); ++m)
+	{
+		if ((m * BLOCK_SIZE + threadIdx.x) < N)
+		{
+			x_shfl_src = S[threadIdx.x + m * BLOCK_SIZE] - 1.0;
+			x_shfl_src *= (double)(threadIdx.x + m * BLOCK_SIZE);
+		}
+		else
+		{
+			x_shfl_src = 0.0;
+		}
+		__syncthreads();
+
+		//        #pragma unroll
+		for (int e = 0; e < 32; ++e)
+		{
+			// --- Column-major ordering - faster
+			x_shfl_dest = __shfl_sync(0xffffffff, x_shfl_src, e);
+			// y_val += d_j0table[tid * nCols + (e + BLOCK_SIZE * m)] * x_shfl_dest;
+			y_val += j0f(double(tid) * dr * double(e + BLOCK_SIZE * m) * dk) * x_shfl_dest;
+			// --- Row-major ordering - slower
+			// y_val += d_V_ph_k[tid * nCols + (e + BLOCK_SIZE * m)] * x_shared[e];
+		}
+
+		__syncthreads();
+	}
+
+	if (tid < N)
+	{
+		sum_diff_g[0] += fabs(1.0 + (y_val * dk * dk / (2.0 * M_PI * rho)) - g[tid]);
+		g[tid] = g[tid] * exp(dt * (1.0 + (y_val * dk * dk / (2.0 * M_PI * rho)) - g[tid]));
+	}
+}
