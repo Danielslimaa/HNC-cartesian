ca25c307228e541bdd0e0199f528cfc710dd4dde
DanielSlimaa <dslima793@gmail.com>
2024-05-30 11:50:56 -0300

    -

--- kernels.cuh
+++ kernels.cuh
@@ -204,7 +204,7 @@ __global__ void g_from_S3(
 
 	double y_val = 0.0;
 
-#pragma unroll
+	#pragma unroll
 	for (unsigned int m = 0; m < ((N + BLOCK_SIZE - 1) / BLOCK_SIZE); ++m)
 	{
 		if ((m * BLOCK_SIZE + threadIdx.x) < N)
@@ -243,14 +243,14 @@ __global__ void ifft_cossine_x_integral(
 	double *__restrict__ g,
 	const double *__restrict__ S,
 	double *sum_diff_g
-    int * d_j)
+  int d_column)
 {
-    int j = *d_j;
+  int j = d_column;
 	const unsigned int tid = threadIdx.x + blockIdx.x * blockDim.x;
 	double x_shfl_src, x_shfl_dest;
 	double y_val = 0.0;
 
-    #pragma unroll
+  #pragma unroll
 	for (unsigned int m = 0; m < ((N + BLOCK_SIZE - 1) / BLOCK_SIZE); ++m)
 	{
 		if ((m * BLOCK_SIZE + threadIdx.x) < N)
@@ -269,7 +269,7 @@ __global__ void ifft_cossine_x_integral(
 			// --- Column-major ordering - faster
 			x_shfl_dest = __shfl_sync(0xffffffff, x_shfl_src, e);
 			// y_val += d_j0table[tid * nCols + (e + BLOCK_SIZE * m)] * x_shfl_dest;
-			y_val += j0f(double(tid) * dr * double(e + BLOCK_SIZE * m) * dk) * x_shfl_dest;
+			y_val += cos(double(tid) * dx * double(e + BLOCK_SIZE * m) * dkx) * x_shfl_dest;
 			// --- Row-major ordering - slower
 			// y_val += d_V_ph_k[tid * nCols + (e + BLOCK_SIZE * m)] * x_shared[e];
 		}
@@ -287,14 +287,14 @@ __global__ void ifft_cossine_y_integral(
 	double *__restrict__ g,
 	const double *__restrict__ S,
 	double *sum_diff_g
-    int * d_i)
+  int d_row)
 {
-    int i = *d_i;
+  int i = d_row;
 	const unsigned int tid = threadIdx.y + blockIdx.y * blockDim.y;
 	double x_shfl_src, x_shfl_dest;
 	double y_val = 0.0;
 
-    #pragma unroll
+  #pragma unroll
 	for (unsigned int m = 0; m < ((N + BLOCK_SIZE - 1) / BLOCK_SIZE); ++m)
 	{
 		if ((m * BLOCK_SIZE + threadIdx.y) < N)
@@ -313,7 +313,7 @@ __global__ void ifft_cossine_y_integral(
 			// --- Column-major ordering - faster
 			x_shfl_dest = __shfl_sync(0xffffffff, x_shfl_src, e);
 			// y_val += d_j0table[tid * nCols + (e + BLOCK_SIZE * m)] * x_shfl_dest;
-			y_val += j0f(double(tid) * dr * double(e + BLOCK_SIZE * m) * dk) * x_shfl_dest;
+			y_val += cos(double(tid) * dy * double(e + BLOCK_SIZE * m) * dky) * x_shfl_dest;
 			// --- Row-major ordering - slower
 			// y_val += d_V_ph_k[tid * nCols + (e + BLOCK_SIZE * m)] * x_shared[e];
 		}
@@ -322,7 +322,7 @@ __global__ void ifft_cossine_y_integral(
 
 	if (tid < N)
 	{
-		g[tid] = 1.0 + (y_val * dk * dk / (2.0 * M_PI * rho));
+		g[tid] = 1.0 + (y_val * dkx * dky / (2.0 * M_PI * 2.0 * M_PI * rho));
 	}
 }
 
@@ -330,14 +330,13 @@ __global__ void fft_cossine_x_integral(
 	double *__restrict__ g,
 	const double *__restrict__ S,
 	double *sum_diff_g
-    int * d_j)
+  int d_column)
 {
-    int j = *d_j;
+  int j = d_column;
 	const unsigned int tid = threadIdx.x + blockIdx.x * blockDim.x;
 	double x_shfl_src, x_shfl_dest;
 	double y_val = 0.0;
-
-    #pragma unroll
+  #pragma unroll
 	for (unsigned int m = 0; m < ((N + BLOCK_SIZE - 1) / BLOCK_SIZE); ++m)
 	{
 		if ((m * BLOCK_SIZE + threadIdx.x) < N)
@@ -356,11 +355,10 @@ __global__ void fft_cossine_x_integral(
 			// --- Column-major ordering - faster
 			x_shfl_dest = __shfl_sync(0xffffffff, x_shfl_src, e);
 			// y_val += d_j0table[tid * nCols + (e + BLOCK_SIZE * m)] * x_shfl_dest;
-			y_val += j0f(double(tid) * dr * double(e + BLOCK_SIZE * m) * dk) * x_shfl_dest;
+			y_val += cos(double(tid) * dx * double(e + BLOCK_SIZE * m) * dky) * x_shfl_dest;
 			// --- Row-major ordering - slower
 			// y_val += d_V_ph_k[tid * nCols + (e + BLOCK_SIZE * m)] * x_shared[e];
 		}
-
 		__syncthreads();
 	}
 
@@ -374,14 +372,13 @@ __global__ void fft_cossine_y_integral(
 	double *__restrict__ g,
 	const double *__restrict__ S,
 	double *sum_diff_g
-    int * d_i)
+  int d_row)
 {
-    int i = *d_i;
+  int i = d_row;
 	const unsigned int tid = threadIdx.y + blockIdx.y * blockDim.y;
 	double x_shfl_src, x_shfl_dest;
 	double y_val = 0.0;
-
-    #pragma unroll
+  #pragma unroll
 	for (unsigned int m = 0; m < ((N + BLOCK_SIZE - 1) / BLOCK_SIZE); ++m)
 	{
 		if ((m * BLOCK_SIZE + threadIdx.y) < N)
@@ -400,7 +397,7 @@ __global__ void fft_cossine_y_integral(
 			// --- Column-major ordering - faster
 			x_shfl_dest = __shfl_sync(0xffffffff, x_shfl_src, e);
 			// y_val += d_j0table[tid * nCols + (e + BLOCK_SIZE * m)] * x_shfl_dest;
-			y_val += j0f(double(tid) * dr * double(e + BLOCK_SIZE * m) * dk) * x_shfl_dest;
+			y_val += cos(double(tid) * dy * double(e + BLOCK_SIZE * m) * dky) * x_shfl_dest;
 			// --- Row-major ordering - slower
 			// y_val += d_V_ph_k[tid * nCols + (e + BLOCK_SIZE * m)] * x_shared[e];
 		}
